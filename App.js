5e2f0f03d30a2f3621516e6329e9932104b07896
Rex Belli Isidore Lumantao <lumantao.rex18@gmail.com>
2024-11-04 23:37:50 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider, Text, TextInput, Button } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View style={{height: 100, alignItems: 'center', justifyContent: 'center', backgroundColor:'#9B7EBD'}}>
+            <Text variant='headlineLarge' style={{color: 'white'}}>Sign Up</Text>
+          </View>
+          <View style={styles.regCont}>
+              <TextInput
+              mode='outlined'
+              label='Firstname'
+              activeOutlineColor='black'
+              style={styles.input}
+              />
+              <TextInput
+              mode='outlined'
+              label='Lastname'
+              activeOutlineColor='black'
+              style={styles.input}
+              />
+              <TextInput
+              mode='outlined'
+              label='Email'
+              activeOutlineColor='black'
+              style={styles.input}
+              />
+              <TextInput
+              mode='outlined'
+              label='Password'
+              activeOutlineColor='black'
+              style={styles.input}
+              />
+              <TextInput
+              mode='outlined'
+              label='Confrim Password'
+              activeOutlineColor='black'
+              style={styles.input}
+              />
+              <Button
+              mode='contained'
+              dark={true}
+              buttonColor='#9B7EBD'
+              style={{width: 300, marginTop: 20}}
+              >
+                Sign Up
+              </Button>
+              <View style={{flexDirection: 'row', marginTop: 20}}>
+                <Text variant='titleMedium' style={{marginTop: 5}}>Already have an account?</Text>
+                <Button
+                mode='text'
+                labelStyle={{fontSize: 17}}
+                >
+                  Click Here
+                </Button>
+              </View>
+          </View>
+          
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: '100%',
+    width: '100%',
+  },
+  regCont:{
+    backgroundColor: 'white',
+    width: '100%',
+    height: 650,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input:{
+    height: 50, 
+    width: 300,
+    backgroundColor: 'white',
+    marginTop: 20,
   },
 });
